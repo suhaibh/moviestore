4f31d54b5338d5f975a5180d37bc8bea49d765a9
Suhaib Habibullah <shabibullah92@gmail.com>
2015-05-10 23:27:53 -0400

    Initialize repo

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,36 @@
+  class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  # GET /movies
+  # GET /movies.json
+  def index
+    @movies = Movie.all
+  end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show
+  end
+
+
+  # DELETE /movies/1
+  # DELETE /movies/1.json
+  def destroy
+    @movie.destroy
+    respond_to do |format|
+      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:movie).permit(:title, :release_year, :price, :description, :imdb_id, :poster_url)
+    end
+end
