d0ed8b6a9a349f99dcc8dda60468ff590e3c9f82
suhaibh <suhaib.habibullah@gmail.com>
2016-01-31 11:56:06 -0500

    Add cart_action method that checks if movie is apart of Redis set

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,4 +8,12 @@ class Movie < ActiveRecord::Base
 	def imdb
 		"http://www.imdb.com/title/#{imdb_id}/"
 	end
+
+	def cart_action(current_user_id)
+		if $redis.sismember "cart#{current_user_id}", id
+			"Remove from"
+		else
+			"Add To"
+		end
+	end
 end
