1cf873f05218a9b550f2f44c9c0c6ae47d67f13c
suhaibh <suhaib.habibullah@gmail.com>
2016-02-08 01:04:21 -0500

    Add movie and purchase associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  has_many :purchases, foreign_key: :buyer_id
+  has_many :movies, through: :purchases
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
